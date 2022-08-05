8b302f53d7b4e29a16241c87f06056f9509541e7
Luke Gibson <lukegibson101@gmail.com>
2022-08-05 14:51:16 +0000

    added add to bag functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -157,7 +157,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIVERY_PERCENTAGE  = 10
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
