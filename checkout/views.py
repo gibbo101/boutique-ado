78af17c4ffd0cbeff29eb7c7416a78124d79187e
Luke Gibson <lukegibson101@gmail.com>
2022-08-15 20:45:05 +0000

    added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LX9oVD0kFC51IAOpvzx5sHKHj91I1c7m1EPifhhOgK2m3dj87HpOXHvTXzXSbwv9W1MemBb0BhyPeHTdDjexQCp00oyJDoLEG',
+        'client_secret': 'sk_test_51LX9oVD0kFC51IAOpScmcCVe8wNYbhFlDEqpZXk5qFinq8peOtjW87E5kwyFBGQPK9hiWgQc468XumDc1b9ILu0r00opAimTGa',
     }
 
     return render(request, template, context)
